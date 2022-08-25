8036062bf0a6238349ff7a03a91ac110d300b13a
Rene90 <rob_rene199027@hotmail.com>
2022-08-25 14:22:05 -0600

    Logout

--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -1,17 +1,28 @@
 import React, {useState} from "react"
 import { Menu } from "antd"
-import { MailOutlined, AppstoreOutlined, SettingOutlined, UserOutlined, UserAddOutlined } from '@ant-design/icons';
+import { MailOutlined, AppstoreOutlined, SettingOutlined, UserOutlined, UserAddOutlined , LogoutOutlined} from '@ant-design/icons';
 import { Link} from "react-router-dom"
 import firebase from "firebase/compat/app"
 const {SubMenu, Item} = Menu
-
-
+import firebase from 'firebase'
+import { useDispatch } from "react-redux";
+import {useHistory} from 'react-router-dom'
 const Header = () =>{
     const [current, setCurrent]= useState("home")
+    let dispatch = useDispatch()
+    let history = useHistory()
     const handleClick = (e)=>{
         setCurrent(e.key)
     }
-
+    const logout  = ()=> {
+        firebase.auth().signOut()
+        dispatch({
+            type:"LOGOUT",
+            payload:null
+        })
+        history.push("/login")
+        
+    }
     return (
         <Menu onClick={handleClick} selectedKeys ={[current]} mode="horizontal" >
             <Item key="home" icon={<AppstoreOutlined />} >
@@ -33,6 +44,9 @@ const Header = () =>{
                 </Item>
                 <Item key="five" icon={<AppstoreOutlined />}>
                 Navigation Five
+                </Item>
+                <Item icon={<LogoutOutlined/>} onClick = {logout} >
+                Logout
                 </Item>
           
             </SubMenu>
