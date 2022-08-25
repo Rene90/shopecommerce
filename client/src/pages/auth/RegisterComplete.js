a0f807b0a30b4397ff49c7f2d3426a72750d2b5b
Rene90 <rob_rene199027@hotmail.com>
2022-08-25 10:09:09 -0600

    Validacion sencilla

--- client/src/pages/auth/RegisterComplete.js
+++ client/src/pages/auth/RegisterComplete.js
@@ -1,19 +1,50 @@
 import React, { useState, useEffect } from "react"
 import { auth } from "../../firebase"
 import {toast} from "react-toastify"
+import { ConsoleSqlOutlined } from "@ant-design/icons"
 
 //import { sendSignInLinkToEmail } from "firebase/auth";
 //import {getAuth, sendSignInLinkToEmail} from "firebase/auth"
 const RegisterComplete= ({history})=> {
     const [email, setEmail] =useState("")
     const [password, setPassword] = useState("")
-    useState(()=>{
+    useEffect(()=>{
         setEmail(window.localStorage.getItem('emailForRegistration'))
+       // console.log(window.location.href)
+        //console.log(window.localStorage.getItem('emailForRegistration'))
+
     },[])
     
     const handleSubmit =async(e)=>{
         e.preventDefault()
-        
+        // validacion
+        if (!email || !password){
+            toast.error('Se requiere email y password')
+            return
+        }
+        if (password.length<6){
+            toast.error("Passsword debe ser al menos de 6 caracteres de largo")
+            return 
+        }
+        try{
+            const result =await auth.signInWithEmailLink(email,window.location.href)
+            //console.log('RESULT',result)
+            if (result.user.emailVerified){
+                //borrar email del local storage
+                window.localStorage.removeItem('emailForRegistration')
+                //obtener el token del usuario
+                let user = auth.currentUser
+                await user.updatePassword(password)
+                const idTokenResult = await user.getIdTokenResult()
+                console.log('user', user, 'idTokenResult',idTokenResult)
+                //redirect
+                history.push('/')
+
+
+            }
+        }catch (error){
+            toast.error(error.message)
+        }
     
     }
     const completeRegistrationForm = () => <form onSubmit={handleSubmit}>
